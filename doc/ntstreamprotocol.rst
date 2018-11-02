48f175e28f58545a19f813ba6b68e84b1cffa6f7
Robert Russell <robertrussell.72001@gmail.com>
2018-11-02 06:10:53 -0700

    Added minimum send size to NTStream. Finished (?) writing NTStream protocol spec.

--- doc/ntstreamprotocol.rst
+++ doc/ntstreamprotocol.rst
@@ -20,14 +20,50 @@ means of communication since, as recommended in the
 a good Network Tables implementation should discard old entry data in favour of new data if the old data has not yet
 been broadcast to other nodes on the Network Tables network. If one were to use the three scalar types for this purpose,
 one would have to transmit a single piece of datum across the entry and wait for a response from the client indicating
-the data was received and the next piece of datum can be sent; for a data source producing data rapidly, this solution
+the datum was received and the next piece of datum can be sent; for a data source producing data rapidly, this solution
 would be far too slow. We thus turn to the array data types, which allow one to efficiently send data in blocks. The
 array solution, however, requires regulation: the spooky-console Network Tables Stream Protocol.
+
+
+Basic Overview
+--------------
+
+The spooky-console Network Tables Stream Protocol is very simple. It works by having the receiving side of a stream
+(hereby to be referred to as the "receiver") use an empty array response as an indicator that the sending side of a
+stream (hereby to be referred to as the "sender") may send the next array of data. In this way, only one Network Tables
+entry is required for unidirectional stream communication; bidirectional communication can be achieved with two entries,
+one for receiving and one for transmitting. Note that this protocol only supports communication between two parties.
+
+There is no default way for a participant in a Network Tables network to identify which entries are streams and which
+are not (i.e. no "handshake" procedure, etc.); this is expected to be known by all participants in advance.
+
+
+Receiving
+---------
+
+The receiver has three basic principles to uphold:
+
+ - To accept any received data from the sender as quickly as possible to permit the transfer of new data. This is
+   critical if the sender employs a cache system because the sender will be forced, due to memory concerns, to
+   eventually start deleting old cached data.
+
+ - To set the Network Tables entry to an empty array after each block of data has been received.
+
+ - To never set the Network Tables entry to anything other than an empty array.
 
 
 Transmitting
 ------------
 
+The sender, being in control of how quickly data is sent, only has one important principle to uphold: to never set the
+entry while there is still data in it.
 
-Receiving
----------
+Although it is not required, the sender should only send data in blocks of a specific minimum size. The exact minimum
+block size will vary, but, in general, should be proportional to the rate of data generated. Following this
+recommendation will help reduce the overhead delays inherent to networking and a potentially delayed response from the
+receiver.
+
+The sender will likely be required to employ a cache system to save any data generated while waiting for a responce from
+the receiver. If the minimum send size recommendation is followed, such a cache system would also prove helpful in that
+data can be added to the cache until a minimum cache size is reached, at which point the entry can be set and the cache
+can be cleared.
--- spookyconsole/nt/stream.py
+++ spookyconsole/nt/stream.py
@@ -18,45 +18,47 @@ class NTStream:
     TODO
     """
 
-    # TODO: send data in specific chunk sizes?
-
-    def __init__(self, kind, receiving_entry=None, transmitting_entry=None, cache_size=100):
+    def __init__(self, kind, receiving_entry=None, transmitting_entry=None,
+                 cache_size=100, min_send_size=0):
         if receiving_entry is None or transmitting_entry is None:
             raise ValueError("at least one entry must be given")
-        self.r_set_func = None
-        self.t_set_func = None
+        if cache_size < min_send_size:
+            raise ValueError("cache_size must be bigger than min_send_size")
+        self.min_send_size = min_send_size
         self.receiving_entry = receiving_entry
         self.transmitting_entry = transmitting_entry
+        self._r_set_func = None
+        self._t_set_func = None
         self._listener_callback_id = None
         self._listener_flags = 0
         self._listeners = []
         if receiving_entry is not None:
-            self.r_set_func = self._check_entry_type(receiving_entry, kind)
+            self._r_set_func = self._check_entry_type(receiving_entry, kind)
         if transmitting_entry is not None:
-            self.t_set_func = self._check_entry_type(transmitting_entry, kind)
+            self._t_set_func = self._check_entry_type(transmitting_entry, kind)
             self.cache = deque(maxlen=cache_size)
+            self.transmitting_entry.addListener(lambda *_: self.flush(False), NetworkTablesInstance.NotifyFlags.UPDATE)
 
     def read(self):
         self._assert_readable("cannot read a write-only stream")
         ret = []
         if self._data_available():
             ret = self.receiving_entry.value
-            self.r_set_func([])
+            self._r_set_func([])
         return ret
 
     def write(self, data):
-        # self._assert_writeable("cannot write to a read-only stream")
+        self._assert_writeable("cannot write to a read-only stream")
         if not isinstance(data, typing.Container) or isinstance(data, str):
             data = [data]
-        self.flush()
         self.cache.extend(data)
-        self.flush()
+        return self.flush(False)
 
     def readable(self):
-        return self.r_set_func is not None
+        return self._r_set_func is not None
 
     def writeable(self):
-        return self.t_set_func is not None
+        return self._t_set_func is not None
 
     def _data_available(self):
         return bool(self.receiving_entry.value)
@@ -64,11 +66,18 @@ class NTStream:
     def _output_blocked(self):
         return bool(self.transmitting_entry.value)
 
-    def flush(self):
-        self._assert_writeable("cannot write to a read-only stream")
-        if not self._output_blocked():
-            self.t_set_func(list(self.cache))
+    def _enough_data_in_cache(self):
+        return len(self.cache) >= self.min_send_size
+
+    def flush(self, force=True):
+        self._assert_writeable("cannot flush a read-only stream")
+        if not self._output_blocked() and (force or self._enough_data_in_cache()):
+            # The networktables array setters make an immutable copy of the passed in data, so we're safe to clear the
+            # deque after.
+            self._t_set_func(self.cache)
             self.cache.clear()
+            return True
+        return False
 
     def peek(self):
         self._assert_readable("cannot read a write-only stream")
@@ -107,7 +116,7 @@ class NTStream:
                     clear_after = True
                 listener.callback(entry, key, value, flags)
         if clear_after:
-            self.r_set_func([])
+            self._r_set_func([])
 
     def _assert_readable(self, msg):
         if not self.readable():
